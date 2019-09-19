78ee63b3c95e1f5066f662a20f48016f10aa4b91
herdanuyp <heh.danu@gmail.com>
2019-09-19 16:49:17 +0700

    Update todo

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,60 @@ const express = require("express");
 const route = express.Router();
 const todos = require("../data/todos");
 
+// ~get all todo~
 route.get("/", (req, res) => {
+  res.send(todos);
+});
+
+// ~get todo by id~
+route.get("/:id", (req, res) => {
   try {
+    const filteredTodo = todos.find(item => item.id == req.params.id);
     res.send({
-      message: `Here your todo list`,
-      todos
+      message: "Here is what you looking for",
+      filteredTodo
     });
   } catch (error) {
     res.send({
-      message: `error get todo`,
+      message: `error get todo by id`,
       error
     });
   }
 });
 
-route.delete("/:id", async (req, res) => {
+// ~add new todo~
+route.post("/", (req, res) => {
   try {
-    let idTodo = await todos.findIndex(
-      element => element.id === Number(req.params.id)
-    );
+    let newId = todos.length + 1;
+    let newTodo = {
+      id: newId,
+      task: req.body.task,
+      done: false
+    };
 
-    todos.splice(idTodo, 1);
+    todos.push(newTodo);
 
-    res.send({
-      message: `deleted todo succeed`,
+    res.status(200).send({
+      message: "todo successfully added",
       todos
     });
+  } catch (error) {
+    res.send({
+      message: `error add todo`,
+      error
+    });
+  }
+});
+
+// ~delete todo by its id~
+route.delete("/:id", (req, res) => {
+  try {
+    const idToDelete = req.params.id;
+    let newTodo = todos.filter(item => item.id !== parseInt(idToDelete));
+
+    todos = newTodo;
+
+    res.status(200).send(todos);
   } catch (error) {
     res.send({
       message: `error delete todo`,
@@ -37,41 +65,23 @@ route.delete("/:id", async (req, res) => {
   }
 });
 
-route.put("/:id", async (req, res) => {
+// ~update a todo by its id~
+route.put("/:id", (req, res) => {
   try {
-    let idTodo = await todos.findIndex(
-      element => element.id === Number(req.params.id)
-    );
+    let getTodoToUpdate = todos.findIndex(data => data.id == req.params.id);
 
     todos.map(data => {
-      if (data.id === idTodo) {
-        todos[idTodo].todo = req.body.todo;
+      if (data.id == req.params.id) {
+        todos[getTodoToUpdate].task = req.body.task;
       }
     });
-
     res.send({
-      message: `todo successfuly updated`,
+      message: "data successfully updated",
       todos
     });
   } catch (error) {
     res.send({
       message: `error update todo`,
-      error
-    });
-  }
-});
-
-route.post("/", (req, res) => {
-  try {
-    todos.push(req.body);
-    res.send({
-      message: `new todo successfuly added`,
-      todos
-    });
-  } catch (error) {
-    console.log(error);
-    res.send({
-      message: `error add todo`,
       error
     });
   }
