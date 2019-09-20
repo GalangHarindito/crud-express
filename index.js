3a4e8b99e197b5e6f63874c8512bac27aabd59e7
herdanuyp <heh.danu@gmail.com>
2019-09-20 10:32:24 +0700

    Add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 
 const app = express();
 const { port, secret } = require("./config/envVariables");
@@ -9,6 +10,7 @@ const PORT = port || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/env", (req, res) => res.send(JSON.stringify(secret)));
 app.use("/", indexRouter);
